a7e678bef7329c55428598ae329afc6c2cc834b2
Giperbola <egor.crut2017@yandex.ru>
2025-09-19 23:16:36 +0300

    Cmake

--- Dropbox/Main.cpp
+++ Dropbox/Main.cpp
@@ -1,6 +1,17 @@
 #include "include/DropboxApp.hpp"
 
 
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+//    LPSTR lpCmdLine, int nCmdShow)
+//{
+//    setlocale(LC_ALL, "");
+//
+//    DropboxApp dropbox_app;
+//    dropbox_app.run();
+//
+//    return 0;
+//}
+
 int main()
 {
 	setlocale(LC_ALL, "");
